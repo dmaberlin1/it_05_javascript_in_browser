0381bd51cb9d0e59efc6bdb2211b310099bcc39e
dmaberlin1 <dmaberlin77@gmail.com>
2023-05-15 12:05:29 +0300

    feat: complete desk and media

--- src/index.js
+++ src/index.js
@@ -1,18 +1,49 @@
 "use strict"
 
 
+
+
 const showAllBtn = document.getElementById('show-all-btn');
 const wrapperCards = document.querySelector('.wrapper-cards');
 let isShowAll = false;
 
 showAllBtn.addEventListener('click', function() {
     if (isShowAll) {
-        wrapperCards.style.maxHeight = '220px';
+        wrapperCards.style.maxHeight = '160px';
         isShowAll = false;
-        showAllBtn.innerHTML='Показать все'
+        // изменяем содержимое первого div
+        showAllBtn.querySelector('div:first-child').innerHTML = `
+          
+            <img src="../src/assets/arrow-down.svg" alt="down">
+           <img src="../src/assets/arrow-down.svg" alt="down">
+        `;
+        // изменяем содержимое второго div
+        showAllBtn.querySelector('div:last-child').textContent = 'Показать все';
     } else {
         wrapperCards.style.maxHeight = 'none';
         isShowAll = true;
-        showAllBtn.innerHTML='Скрыть'
+        // изменяем содержимое первого div
+        showAllBtn.querySelector('div:first-child').innerHTML = `
+           <img src="../src/assets/up-arrow.svg" alt="down">
+           <img src="../src/assets/up-arrow.svg" alt="down">
+        `;
+        // изменяем содержимое второго div
+        showAllBtn.querySelector('div:last-child').textContent = 'Скрыть';
     }
 });
+
+
+
+
+
+// showAllBtn.addEventListener('click', function() {
+//     if (isShowAll) {
+//         wrapperCards.style.maxHeight = '220px';
+//         isShowAll = false;
+//         showAllBtn.innerHTML='Показать все'
+//     } else {
+//         wrapperCards.style.maxHeight = 'none';
+//         isShowAll = true;
+//         showAllBtn.innerHTML='Скрыть'
+//     }
+// });
